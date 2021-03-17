ea22ed3b58fb07b781aa8e6b9332050661af87ca
Tech Helper <68120313+Tech-Helper503@users.noreply.github.com>
2021-03-17 05:27:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import threading
+import requests
+
+
+def download():
+  def get_requests():
+    global num_downloads
+    global num_money
+    
+    _r = requests.get('https://mdundo.com/dl/1306447')
+    num_downloads += 1
+    num_money += 0.2
+    
+  
+  
+  while True:
+    t = threading.Thread(target=get_request)
+    t.start()
+    
+    
+if __name__ == '__main__':
+  for _i in range(10):
+      t = threading.Thread(target=download)
